85749e549df3e608aafb36ad0252b1ad3c6f8981
TristanGomez44 <tristan.gomez@etu.univ-nantes.fr>
2019-08-08 14:55:18 +0200

    Set epoch number to 200

--- code/mnist.sh
+++ code/mnist.sh
@@ -1,5 +1,4 @@
-python trainVal.py -c model.config --exp_id mnist --model_id cnn             --epochs 100 --dataset MNIST --model cnn
-python trainVal.py -c model.config --exp_id mnist --model_id gnn_resnet      --epochs 100 --dataset MNIST --model gnn_resnet
+python trainVal.py -c model.config --exp_id mnist --model_id cnn     --epochs 200 --dataset MNIST --model cnn
 #The batch size of the gnn_resnet_mc has to be reduced because of a bug in pytorch
-python trainVal.py -c model.config --exp_id mnist --model_id gnn_resnet_mc   --epochs 200 --dataset MNIST --model gnn_resnet_mc --batch_size 32 --test_batch_size 32
-python trainVal.py -c model.config --exp_id mnist --model_id gcnn_resnet     --epochs 200 --dataset MNIST --model gcnn_resnet
+python trainVal.py -c model.config --exp_id mnist --model_id gnn     --epochs 200 --dataset MNIST --model gnn_resnet_mc --batch_size 32 --test_batch_size 32
+python trainVal.py -c model.config --exp_id mnist --model_id gcnn    --epochs 200 --dataset MNIST --model gcnn_resnet
